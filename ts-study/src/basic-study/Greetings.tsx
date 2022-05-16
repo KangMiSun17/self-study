ad32bd195ad6e1c37bb95d3dd642336e87ba6e04
ms <altjsvnf@naver.com>
2022-05-16 19:14:07 +0900

    study event handler

--- ts-study/src/basic-study/Greetings.tsx
+++ ts-study/src/basic-study/Greetings.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 
+//React.FC 를 사용하지 않는 걸 권장
 type GreetingsProps = {
   name: string;
+  mark: string;
+  optional?: string;
+  onClick: (name: string) => void;
 };
 
-function Greetings({ name }: GreetingsProps) {
-  return <div>Hello, {name}</div>;
+function Greetings({ name, mark, onClick }: GreetingsProps) {
+  const handleClick = () => onClick(name);
+  return (
+    <div>
+      Hello, {name} {mark}
+      <div>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+    </div>
+  );
 }
+
+Greetings.defaultProps = {
+  mark: "!",
+};
 
 export default Greetings;
--- ts-study/src/basic-study/MyForm.tsx
+++ ts-study/src/basic-study/MyForm.tsx
@@ -1,5 +1,31 @@
-function MyForm() {
-  return <div>폼입니다.</div>;
+import { useState } from "react";
+
+type MyFormProps = {
+  onSubmit: (form: { name: string; description: string }) => void;
+};
+
+function MyForm({ onSubmit }: MyFormProps) {
+  const [form, setForm] = useState({ name: "", description: "" });
+
+  const { name, description } = form;
+
+  const onChange = (e: any) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    onSubmit(form);
+    setForm({ name: "", description: "" });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="name" value={name} onChange={onChange} />
+      <input name="description" value={description} onChange={onChange} />
+      <button type="submit">등록</button>
+    </form>
+  );
 }
 
 export default MyForm;
