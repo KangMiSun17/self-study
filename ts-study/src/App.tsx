c5c6da58e8e7c8b640aebf478c599bba1d08532f
ms <altjsvnf@naver.com>
2022-05-16 19:35:04 +0900

    study useReducer typescript

--- ts-study/src/App.tsx
+++ ts-study/src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Counter from "./basic-study/Counter";
 import Greetings from "./basic-study/Greetings";
+import MyForm from "./basic-study/MyForm";
+import ReducerSample from "./basic-study/ReducerSample";
 
 function App() {
+  const onClick = (name: string) => {
+    alert(`${name} hi!`);
+  };
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <div className="App">
-      <Greetings name="MM" />
+      <Greetings name="MM" onClick={onClick} />
       <Counter />
+      <MyForm onSubmit={onSubmit} />
+      <ReducerSample />
     </div>
   );
 }
--- ts-study/src/basic-study/Counter.tsx
+++ ts-study/src/basic-study/Counter.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
-function Button() {
-  const [count, setCount] = useState<number>(0); //Generics 생략해도 상관없음
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
+type Action = { type: "INCREASE" } | { type: "DECREASE" };
+
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    default:
+      throw new Error("Unhandled action");
+  }
+}
+function Counter() {
+  // const [count, setCount] = useState(0); useState로 상태관리 하기
+  const [count, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: "INCREASE" });
+  const onDecrease = () => dispatch({ type: "DECREASE" });
   return (
     <div>
       <p>{count}</p>
@@ -13,4 +26,4 @@ function Button() {
   );
 }
 
-export default Button;
+export default Counter;
