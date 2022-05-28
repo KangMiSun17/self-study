f2f5f58816340e86093d50210c61bcec3b2d27bd
ms <altjsvnf@naver.com>
2022-05-28 23:56:53 +0900

    study react-snap

--- react-study/src/index.js
+++ react-study/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { hydrate } from "react-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const roots = document.getElementById("root");
+
+if (roots.hasChildNodes()) {
+  hydrate();
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
