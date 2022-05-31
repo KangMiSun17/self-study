91ed3a9677c98d89d90c5d2008abdd7d3016ce21
ms <altjsvnf@naver.com>
2022-05-31 19:59:16 +0900

    study enum type

---
+++ ts-study/src/type-study/enum.tsx
@@ -0,0 +1,12 @@
+// enum 선언
+enum Car {
+  BUS = "bus",
+  TAXI = "taxi",
+  SUV = "suv",
+}
+
+// enum Car를 이용해서 taxi값을 할당해주세요.
+const taxi: Car = Car.TAXI;
+console.log(taxi); // TAXI 출력
+
+export {};
