b883ffd83c5d0148b69841ffca4df1bae5f060fe
Kang Mi Sun <altjsvnf@naver.com>
2026-03-25 22:26:40 +0900

    feat: 변수 기본 이론

--- java-study/src/section1/Chapter0.java
+++ java-study/src/section1/Chapter0.java
@@ -3,6 +3,6 @@ package section1;
 public class Chapter0 {
    static void main(String[] args) {
        System.out.println("안녕하세요 강미선입니다.");
-       System.out.println("반갑습니다.");
+       System.out.println("1 + 1 = " + 1 + 1);
    }
 }
