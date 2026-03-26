ead2729e6d8f8a484e4842b1d8b5464b6f6fc422
Kang Mi Sun <altjsvnf@naver.com>
2026-03-26 22:19:25 +0900

    feat: else-if문

---
+++ java-study/src/section3/Chapter5.java
@@ -0,0 +1,14 @@
+package section3;
+
+public class Chapter5 {
+    static void main(String[] args) {
+        int score = 90;
+        if(score >= 81){
+            System.out.println("A등급입니다.");
+        } else if (score >= 61) {
+            System.out.println("B등급입니다.");
+        } else {
+            System.out.println("C등급입니다.");
+        }
+    }
+}
