1de648cdd2add8662146650e5132ba3e6dc186c3
Kang Mi Sun <altjsvnf@naver.com>
2026-03-26 22:30:19 +0900

    feat: while문

---
+++ java-study/src/section3/Chapter7.java
@@ -0,0 +1,14 @@
+package section3;
+
+public class Chapter7 {
+    static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while (i <= 7){
+            System.out.println(i);
+            sum += i;
+            i++;
+        }
+        System.out.println("sum : " + sum);
+    }
+}
