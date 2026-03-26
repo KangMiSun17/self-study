233b37f41dde42d6d3399912e361a115cb8d63e9
Kang Mi Sun <altjsvnf@naver.com>
2026-03-26 22:19:11 +0900

    feat: if문

---
+++ java-study/src/section3/Chapter4.java
@@ -0,0 +1,13 @@
+package section3;
+
+public class Chapter4 {
+    static void main(String[] args) {
+        int age = 20;
+        int money = 2000;
+        if (age >= 19 && money >= 2000) {
+            System.out.println("주류 구매 가능");
+        } else {
+            System.out.println("주류 구매 불가");
+        }
+    }
+}
