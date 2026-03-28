a85fe691041e2353006d53c3d6c8989f47d53aad
Kang Mi Sun <altjsvnf@naver.com>
2026-03-28 18:09:50 +0900

    feat: 객체의 생성

---
+++ java-study/src/section4/Chapter14.java
@@ -0,0 +1,8 @@
+package section4;
+
+public class Chapter14 {
+    static void main(String[] args) {
+        Dog dog1 = new Dog("이지", "진돗개", 25);
+        Dog dog2 = new Dog("아롱이", "시고르자브르", 5);
+    }
+}
---
+++ java-study/src/section4/Dog.java
@@ -0,0 +1,21 @@
+package section4;
+
+public class Dog {
+    String name;
+    String type;
+    int weight;
+
+    public Dog(String name, String type, int weight) {
+        this.name = name;
+        this.type = type;
+        this.weight = weight;
+    }
+
+    // ex) 강아지 미용샵
+    // String furColor;
+    // int furLength;
+
+    public void eat() {
+        System.out.println("냠냠");
+    }
+}
