c9c0964fe7ac38fe9a0b13cfebbd04f4cab367f5
Kang Mi Sun <altjsvnf@naver.com>
2026-03-28 14:47:06 +0900

    feat: do while문

---
+++ java-study/src/section3/Chapter9.java
@@ -0,0 +1,11 @@
+package section3;
+
+public class Chapter9 {
+    static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 5);
+    }
+}
