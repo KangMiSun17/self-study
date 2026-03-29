11ef5d1856b303eb5486a67babcda65361bb0a32
Kang Mi Sun <altjsvnf@naver.com>
2026-03-29 15:47:29 +0900

    feat: 배열

---
+++ java-study/src/section4/Chapter20.java
@@ -0,0 +1,15 @@
+package section4;
+
+public class Chapter20 {
+    static void main(String[] args) {
+//        int[] numbers = new int[3];
+//        numbers[0] = 10;
+//        numbers[1] = 20;
+//        numbers[2] = 30;
+        int[] numbers = {10, 20, 30};
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
